81831af45bcab89992fe8d4c664efc040dddefdc
Igor <houwat@gmail.com>
2021-03-21 19:56:02 -0400

    scss imports and laid out header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 // carbon-components-angular default imports
-import { UIShellModule, IconModule, HeaderModule, HeaderNavigation } from 'carbon-components-angular';
-import {NotificationModule} from '@carbon/icons-angular';
-import {UserAvatarModule} from '@carbon/icons-angular';
-import {AppSwitcherModule} from '@carbon/icons-angular';
-
+import { UIShellModule, ButtonModule, SearchModule, GridModule } from 'carbon-components-angular';
+import {SendAltModule, AddAltModule, HomeModule} from '@carbon/icons-angular';
 
 @NgModule({
   declarations: [
@@ -21,13 +18,15 @@ import {AppSwitcherModule} from '@carbon/icons-angular';
     BrowserModule,
     AppRoutingModule,
     UIShellModule,
-    IconModule,
-    NotificationModule,
-    UserAvatarModule,
-    AppSwitcherModule,
+    SendAltModule,
+    AddAltModule,
+    HomeModule,
+    ButtonModule,
+    SearchModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}

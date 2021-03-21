f4c54d9886f8f002a52ca8c712ff349a322156e5
Igor <houwat@gmail.com>
2021-03-20 21:36:54 -0400

    installed carbon design, added header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule, IconModule, HeaderModule, HeaderNavigation } from 'carbon-components-angular';
+import {NotificationModule} from '@carbon/icons-angular';
+import {UserAvatarModule} from '@carbon/icons-angular';
+import {AppSwitcherModule} from '@carbon/icons-angular';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UIShellModule,
+    IconModule,
+    NotificationModule,
+    UserAvatarModule,
+    AppSwitcherModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
